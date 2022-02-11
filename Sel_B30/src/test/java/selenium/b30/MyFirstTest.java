0a966343a37613c3e48fa68b2b3c0717057721eb
Jane Shalnova <e.l.shalnova@gmail.com>
2022-02-11 15:42:54 +0300

    Исправлена остановка драйвера

--- Sel_B30/src/test/java/selenium/b30/MyFirstTest.java
+++ Sel_B30/src/test/java/selenium/b30/MyFirstTest.java
@@ -8,6 +8,6 @@ public class MyFirstTest {
     public static void main(String[] args){
         WebDriver driver = new ChromeDriver();
         driver.get("https://ru.wikipedia.org/wiki");
-        driver.close();
+        driver.quit();
     }
 }
