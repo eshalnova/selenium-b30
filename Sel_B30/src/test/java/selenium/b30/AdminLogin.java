8142e6488af2e272bd09c26c284d7cf0d214a7d6
Jane Shalnova <e.l.shalnova@gmail.com>
2022-02-14 12:59:37 +0300

    Лекция 2, Задание 3

---
+++ Sel_B30/src/test/java/selenium/b30/AdminLogin.java
@@ -0,0 +1,23 @@
+package selenium.b30;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedCondition;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+public class AdminLogin {
+
+
+    public static void main(String args[]){
+        WebDriver driver = new ChromeDriver();
+        WebDriverWait wait = new WebDriverWait(driver, 10);
+        driver.get("http://localhost/litecart/admin/login.php");
+        driver.findElement(By.name("username")).sendKeys("admin");
+        driver.findElement(By.name("password")).sendKeys("admin");
+        driver.findElement(By.name("login")).click();
+        wait.until(ExpectedConditions.urlToBe("http://localhost/litecart/admin/"));
+        driver.quit();
+    }
+}
